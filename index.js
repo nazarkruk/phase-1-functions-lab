f6f0ec29b73b04092b90d35e16907142c5fa70c5
Nazar Kruk <nazarkruk@icloud.com>
2022-04-28 15:06:26 -0400

    commit tree 28f60f1cfa3657ccd080d5d0941281ad911c8431

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickUp){
+    return Math.abs(pickUp - 42)
+}
+
+function distanceFromHqInFeet(pickUp){
+    let distance  = Math.abs(pickUp - 42)
+    return distance * 264
+}
+
+function distanceTravelledInFeet(start, destination){
+    return (Math.abs(destination - start)) * 264;
+}
+
+function calculatesFarePrice(start, destination){
+    let feedDist = distanceTravelledInFeet(start, destination);
+    if (feedDist<=400){
+        return 0;
+    }else if (feedDist<=2000){
+        return (feedDist - 400)*0.02; 
+    }else if (feedDist<=2500){
+        return 25;
+    }else {
+        return 'cannot travel that far'
+    }
+
+}
